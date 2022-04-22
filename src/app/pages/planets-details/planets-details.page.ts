f9de78c838a519ede6fd42b7d7f10a49d5a06930
Carlos Eduardo Carmo do Nascimento <karloseduardo19@hotmail.com>
2022-04-22 13:21:31 +0000

    CA2

--- src/app/pages/planets-details/planets-details.page.ts
+++ src/app/pages/planets-details/planets-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-planets-details',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planets-details.page.scss'],
 })
 export class PlanetsDetailsPage implements OnInit {
-
-  constructor() { }
-
+	
+  planets: any;
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+ 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/planets/${id}`).subscribe(res => {
+      this.planets = res;
+    });
   }
 
 }
